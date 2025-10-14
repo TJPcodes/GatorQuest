0f9ec0bd163e924e9b2ae8eaeb13eafbf26b9add
emiclaire117 <emiliadelaune@ufl.edu>
2025-10-14 10:49:48 -0400

    Initialized Angular Setup

---
+++ frontend/gator-quest-app/src/app/home/text-box/text-box.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-text-box',
+  imports: [],
+  templateUrl: './text-box.html',
+  styleUrl: './text-box.css'
+})
+export class TextBox {
+
+}
