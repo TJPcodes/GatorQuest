85bd68469fb1ce9663f75ce5997fad66ddc91872
emiclaire117 <emiliadelaune@ufl.edu>
2025-11-05 12:12:53 -0500

    Added text-box for user

--- frontend/gator-quest-app/src/app/home/text-box/text-box.ts
+++ frontend/gator-quest-app/src/app/home/text-box/text-box.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { InputTextModule } from 'primeng/inputtext';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-text-box',
-  imports: [],
+  imports: [ InputTextModule, CommonModule, FormsModule ],
   templateUrl: './text-box.html',
-  styleUrl: './text-box.css'
+  styleUrl: './text-box.css',
+  standalone: true
 })
 export class TextBox {
-
+  userInput: string = '';
 }
